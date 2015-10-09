4ec7df1d94cdca91bdd23e998b8a379fe180f3a0
Pelumi Aboluwarin <lumzor@gmail.com>
2015-10-09 19:22:02 +0100

    refactoring and addition of listings data

--- com/labs/sortable/read_input.py
+++ com/labs/sortable/read_input.py
@@ -3,25 +3,33 @@ __author__ = 'pelumi'
 import pandas as pd
 
 PRODUCTS_FILE = "../../../data/products.txt"
-LISTINGS_FILE = "../../../data/products.txt"
+LISTINGS_FILE = "../../../data/listings.txt"
 
-# read the entire file into a python array
-with open(PRODUCTS_FILE, 'rb') as f:
-    data = f.readlines()
+def loadJsonToDf(filename):
+    # read the entire file into a python array
+    with open(filename, 'rb') as f:
+        data = f.readlines()
 
-# remove the trailing "\n" from each line
-data = map(lambda x: x.rstrip(), data)
-data_json_str = "[" + ','.join(data) + "]"
+    # remove the trailing "\n" from each line
+    data = map(lambda x: x.rstrip(), data)
+    data_json_str = "[" + ','.join(data) + "]"
 
-# now, load it into pandas
-products_df = pd.read_json(data_json_str)
+    # now, load it into a pandas data frame
+    df = pd.read_json(data_json_str)
+    return df
 
-print products_df.head(10) # df.name.ravel()
+products_df = loadJsonToDf(PRODUCTS_FILE)
+listings_df = loadJsonToDf(LISTINGS_FILE)
+
+print products_df.head(10)
+print listings_df.head(10)
 
 all_manufacturers = pd.unique(products_df.manufacturer.ravel())
-
-
-
+all_model_numbers = pd.unique(products_df.model.ravel())
+families = pd.unique(products_df.family.ravel())
 
 print all_manufacturers
-print all_manufacturers.size
+print ("Total manufacturers is: ", all_manufacturers.size)
+print ("Total families is: ", families.size)
+print ("Total models is: ", all_model_numbers.size)
+
